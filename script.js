ecfae941e3e072771bf31df92467c569fe2a9f7c
IsaacRaffs <124933998+IsaacRaffs@users.noreply.github.com>
2023-09-28 09:52:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('btn').onclick = function (){
+    a = document.getElementById("bodi");
+    a.style.background = 'black'
+}
+document.getElementById('btnwhite').onclick = function (){
+    a = document.getElementById("bodi");
+    a.style.background = 'white'
+}
